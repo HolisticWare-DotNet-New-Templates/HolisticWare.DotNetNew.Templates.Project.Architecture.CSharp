c3d62b4c172f8a47f04070fb477826a536eb6e71
moljac <mcvjetko@holisticware.net>
2023-08-01 19:56:38 +0200

    net6.0 libraries

---
+++ Content/source/UserInterface/net6.0/ProjectsStructureTemplate.UserInterface.Library.MAUI/Pages/PageWebView.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectsStructureTemplate.UserInterface.Library.MAUI.Pages;
+
+public partial class PageWebView : ContentPage
+{
+	public PageWebView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Content/source/UserInterface/net6.0/ProjectsStructureTemplate.UserInterface.Library.MAUI/Views/WebView.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectsStructureTemplate.UserInterface.Library.MAUI.Views;
+
+public partial class WebView :	ContentView
+								//Microsoft.Maui.Controls.WebView
+{
+	public WebView()
+	{
+		InitializeComponent();
+	}
+
+    protected void webview_Navigating(System.Object sender, Microsoft.Maui.Controls.WebNavigatingEventArgs e)
+    {
+        return;
+    }
+
+    protected void webview_Navigated(System.Object sender, Microsoft.Maui.Controls.WebNavigatingEventArgs e)
+    {
+        return;
+    }
+}
