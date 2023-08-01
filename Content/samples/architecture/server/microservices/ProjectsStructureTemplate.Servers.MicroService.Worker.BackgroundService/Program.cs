3ca379c084e6ce322b6937ba9c003dcd2b988299
moljac <mcvjetko@holisticware.net>
2023-08-01 19:55:37 +0200

    cleanup

--- Content/samples/architecture/server/microservices/ProjectsStructureTemplate.Servers.MicroService.Worker.BackgroundService/Program.cs
+++
@@ -1,7 +0,0 @@
-using ProjectsStructureTemplate.Servers.MicroService.Worker.BackgroundService;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
